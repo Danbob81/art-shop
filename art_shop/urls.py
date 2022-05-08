8c0d199014f3b9b888d40f4d2f7957810603c51d
Daniel Higginson <danbobhig@gmail.com>
2022-05-08 20:10:16 +0000

    display product images and names in products template

--- art_shop/urls.py
+++ art_shop/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +26,4 @@ urlpatterns = [
     path('gallery/', include('gallery.urls')),
     path('shop/', include('shop.urls')),
     path('products/', include('products.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,7 +1,14 @@
 """ A view to return the products template """
 from django.shortcuts import render
+from .models import Product
 
 
 def products(request):
-    """ Return shop page """
-    return render(request, 'products/products.html')
+    """ Return shop page, showing products """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/products.html', context)
