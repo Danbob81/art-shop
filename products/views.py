6eca07b32461bc3597f019bbece06ffe8973a4dc
Daniel Higginson <danbobhig@gmail.com>
2022-05-09 20:02:10 +0000

    add filter by category functionality to shop menus

--- products/views.py
+++ products/views.py
@@ -1,15 +1,23 @@
 """ A view to return the products template """
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
 
 
 def all_products(request):
     """ Return shop page and show all products """
 
     products = Product.objects.all()
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     context = {
         'products': products,
+        'current_categories': categories,
     }
     return render(request, 'products/products.html', context)
 
