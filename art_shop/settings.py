37f2716dc24aa89ea1a999f3c4a136fc0abd334e
Daniel Higginson <danbobhig@gmail.com>
2022-05-10 15:05:34 +0000

    create basket context

--- art_shop/settings.py
+++ art_shop/settings.py
@@ -78,6 +78,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'basket.contexts.basket_contents',
             ],
         },
     },
@@ -162,6 +163,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ basket/contexts.py
@@ -0,0 +1,23 @@
+"""Basket context"""
+from decimal import Decimal
+from django.conf import settings
+
+
+def basket_contents(request):
+    """basket contents"""
+
+    basket_items = []
+    total = 0
+    product_count = 0
+    delivery = total * Decimal(settings.DELIVERY_PERCENTAGE / 100)
+    grand_total = delivery + total
+
+    context = {
+        'basket_items': basket_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'grand_total': grand_total,
+    }
+
+    return context
