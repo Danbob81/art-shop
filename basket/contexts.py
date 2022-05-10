4479d4c71a983507deeed62cca2d438f3ffcdf37
Daniel Higginson <danbobhig@gmail.com>
2022-05-10 20:47:48 +0000

    add functionality to view items in basket ready for user to checkout

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,6 +1,8 @@
 """Basket context"""
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def basket_contents(request):
@@ -11,6 +13,17 @@ def basket_contents(request):
     product_count = 0
     delivery = total * Decimal(settings.DELIVERY_PERCENTAGE / 100)
     grand_total = delivery + total
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     context = {
         'basket_items': basket_items,
