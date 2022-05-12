e523f8f7c9c8839628e7b242cc5d6990d0484e9b
Daniel Higginson <danbobhig@gmail.com>
2022-05-12 14:10:21 +0000

    add increase/decrease quantity and remove item functionality to basket

--- basket/urls.py
+++ basket/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
-    path('add/<item_id>', views.add_to_basket, name='add_to_basket'),
+    path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
+    path('update/<item_id>/', views.update_basket, name='update_basket'),
+    path('remove/<item_id>/', views.remove_from_basket,
+         name='remove_from_basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,5 @@
 """ A view to return the shopping basket template """
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_basket(request):
@@ -32,3 +32,50 @@ def add_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def update_basket(request, item_id):
+    """ Update quantity of a product in the shopping basket """
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    basket = request.session.get('basket', {})
+
+    if size:
+        if quantity > 0:
+            basket[item_id]['items_by_size'][size] = quantity
+        else:
+            del basket[item_id]['items_by_size'][size]
+            if not basket[item_id]['items_by_size']:
+                basket.pop(item_id)
+    else:
+        if quantity > 0:
+            basket[item_id] = quantity
+        else:
+            basket.pop(item_id)
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+
+def remove_from_basket(request, item_id):
+    """ Remove a product from the shopping basket """
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        basket = request.session.get('basket', {})
+
+        if size:
+            del basket[item_id]['items_by_size'][size]
+            if not basket[item_id]['items_by_size']:
+                basket.pop(item_id)
+        else:
+            basket.pop(item_id)
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
