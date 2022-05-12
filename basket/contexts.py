3acd09a7802380c53fd3c7a9ac49a9fbe66ec731
Daniel Higginson <danbobhig@gmail.com>
2022-05-12 10:43:36 +0000

    add increase/decrease quantity buttons and js functionality to product details page

--- basket/contexts.py
+++ basket/contexts.py
@@ -15,15 +15,27 @@ def basket_contents(request):
     grand_total = delivery + total
     basket = request.session.get('basket', {})
 
-    for item_id, quantity in basket.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        basket_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in basket.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            basket_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                basket_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
 
     context = {
         'basket_items': basket_items,
