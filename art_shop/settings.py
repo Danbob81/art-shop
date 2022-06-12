8925fd2aab3ed0e39d799ac0e6793471c5e7747e
Daniel Higginson <danbobhig@gmail.com>
2022-06-12 21:20:59 +0000

    remove secret key and set debug

--- art_shop/settings.py
+++ art_shop/settings.py
@@ -22,12 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = [
-    'django-insecure-e2u$hhfvhh9-ssh-4ks#921$v(tp2qz-^8qry*l%51heim(c@1'
-    ]
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['danbob81-art-shop.heroku.com', 'localhost']
 
