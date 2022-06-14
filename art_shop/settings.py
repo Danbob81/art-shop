ed34e7b77128d273f7a8f7d322f797b152d5120d
Daniel Higginson <danbobhig@gmail.com>
2022-06-14 09:33:13 +0000

    change allowed hosts setting

--- art_shop/settings.py
+++ art_shop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['https://danbob81-art-shop.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['danbob81-art-shop.herokuapp.com', 'localhost']
 
 
 # Application definition
