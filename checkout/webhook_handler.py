6da524c158b8061fd8cb67d9a10a2a76ae19d6e6
Daniel Higginson <danbobhig@gmail.com>
2022-06-19 11:11:00 +0000

    correct error in webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -63,7 +63,7 @@ class StripeWHHandler:
         # Update profile information if save_info was checked
         profile = None
         username = intent.metadata.username
-        if username != 'AnanymousUser':
+        if username != 'AnonymousUser':
             profile = UserProfile.objects.get(user__username=username)
             if save_info:
                 profile.default_phone_number = shipping_details.phone
